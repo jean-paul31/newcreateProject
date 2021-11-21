19dda519f652a89efcbf7cf1c2d409b1d8f2eaf2
jean-paul31 <andrei.jpaul@gmail.com>
2021-11-21 12:12:21 +0100

    ajout du front de l'import

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-require "./view/login.php";
+require './view/navbar.php';
+
+if ($_SESSION['id']) 
+{
+    require "./view/import.php";
+}
+else
+{
+    require "./view/login.php";
+}
+
+require "./view/foot.php";
---
+++ view/import.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row center">
+        <div class="col">
+            <div class="col-md-3 offset-4 center-block  my-auto text-center login">
+                <form>
+                    <div class="mb-3">
+                        <label for="mailConnect" class="form-label">Nom du projet :</label>
+                        <input type="text" class="form-control" name="mailConnect" id="mailConnect">
+                    </div>
+                    <div class="mb-3">
+                        <label for="mailConnect" class="form-label">Instance :</label>
+                        <select name="instance" id="">
+                            <option value="1">POC</option>
+                            <option value="2">EU</option>
+                            <option value="3">FR</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="mailConnect" class="form-label">Nom du projet :</label>
+                        <input type="file" class="form-control" name="mailConnect" id="mailConnect" placeholder="Fichier de paramétrage">
+                        <p>Ne sont accepté que les fichiers excel (.xls)</p>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="Créer">Créer le projet</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
